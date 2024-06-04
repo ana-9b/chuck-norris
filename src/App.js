0fa2a85ea43d887fb09ceaffffbe95f8bfd20b38
ana-9b <anatutic9@gmail.com>
2024-06-05 00:04:07 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Card from './components/Card';
 
 function App() {
+  const [jokes, setJokes] = useState([]);
+
+
+  useEffect(() => {
+    const url = "https://api.chucknorris.io/jokes/random";
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.value);
+      setJokes(data.value);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://market.enonic.com/vendors/enonic/chuck-norris-widget/_/attachment/inline/1c700535-289d-4563-883f-cb8d4ec69b6a:43f4099ef34bd5fa69c7ab886d971be70cba17db/application.svg" className="Chuck-icon" alt="chuck" height={200} />
+
+    <Card>
+      {jokes}
+    </Card>
+        
       </header>
     </div>
   );
---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+export default function Card ({children}) {
+  return (
+    <div className="card">
+      <div>{children}</div>
+    </div>
+  );
+}
